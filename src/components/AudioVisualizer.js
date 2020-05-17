cbd49675fc4dacefe2b81ed688c16536dd2cfae8
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2020-05-17 12:15:11 +0700

    Handle to load audio after user gesture

--- src/components/AudioVisualizer.js
+++ src/components/AudioVisualizer.js
@@ -6,12 +6,11 @@ import { sampleAudio2 } from "../sounds";
 import { useFrame } from "react-three-fiber";
 import * as dat from "dat.gui";
 
-// Init a static Audio Object for the sample audio
-const listener = new THREE.AudioListener();
-const audio = new THREE.Audio(listener);
-audio.setMediaElementSource(sampleAudio2);
-
 const AudioVisualizer = () => {
+  const [userInit, setUserInit] = useState(false);
+  const [listener, setListener] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const [analyser, setAnalyzer] = useState(null);
   const [settings, setSettings] = useState({
     isPlaying: false,
     barColor: 0xffff00,
@@ -43,40 +42,58 @@ const AudioVisualizer = () => {
 
   useEffect(() => {
     if (settings.isPlaying) {
-      sampleAudio2.play();
-      console.log("Play Music");
+      if (!userInit && !listener && !audio) {
+        setUserInit(true);
+      } else {
+        sampleAudio2.play();
+        console.log("Play Music");
+      }
     } else {
       sampleAudio2.pause();
       console.log("Pause Music");
     }
-  }, [settings.isPlaying]);
+  }, [settings.isPlaying, userInit, listener, audio]);
+
+  useEffect(() => {
+    if (userInit) {
+      const _listener = new THREE.AudioListener();
+      const _audio = new THREE.Audio(_listener);
+      _audio.setMediaElementSource(sampleAudio2);
+      setListener(_listener);
+      setAudio(_audio);
+    }
+  }, [userInit]);
 
   // Audio Analyser
   // We will re-init it when the fftSize change
-  const analyser = useMemo(
-    () => new THREE.AudioAnalyser(audio, settings.fftSize),
-    [settings.fftSize]
-  );
+  useEffect(() => {
+    if (audio) {
+      setAnalyzer(new THREE.AudioAnalyser(audio, settings.fftSize));
+    }
+  }, [settings.fftSize, audio]);
 
   // Uniforms to pass into the shader
-  const uniforms = useMemo(
-    () => ({
-      tAudioData: {
-        value: new THREE.DataTexture(
-          analyser.data,
-          settings.fftSize / 2,
-          1,
-          THREE.LuminanceFormat
-        ),
-      },
-      barColor: { type: "vec3", value: new THREE.Color(settings.barColor) },
-    }),
-    [settings, analyser]
-  );
+  const uniforms = useMemo(() => {
+    if (analyser) {
+      return {
+        tAudioData: {
+          value: new THREE.DataTexture(
+            analyser.data,
+            settings.fftSize / 2,
+            1,
+            THREE.LuminanceFormat
+          ),
+        },
+        barColor: { type: "vec3", value: new THREE.Color(settings.barColor) },
+      };
+    }
+  }, [settings, analyser]);
 
   useFrame(() => {
-    analyser.getFrequencyData();
-    uniforms.tAudioData.value.needsUpdate = true;
+    analyser && analyser.getFrequencyData();
+    if (uniforms) {
+      uniforms.tAudioData.value.needsUpdate = true;
+    }
   });
 
   return (
@@ -85,19 +102,21 @@ const AudioVisualizer = () => {
         <boxBufferGeometry attach="geometry" args={[1, 1, 0.2]} />
         <meshBasicMaterial attach="material" color={0x121212} />
       </mesh>
-      <mesh>
-        <planeBufferGeometry attach="geometry" args={[1, 1]} />
-        <shaderMaterial
-          attach="material"
-          args={[
-            {
-              uniforms,
-              vertexShader,
-              fragmentShader,
-            },
-          ]}
-        />
-      </mesh>
+      {uniforms && (
+        <mesh>
+          <planeBufferGeometry attach="geometry" args={[1, 1]} />
+          <shaderMaterial
+            attach="material"
+            args={[
+              {
+                uniforms,
+                vertexShader,
+                fragmentShader,
+              },
+            ]}
+          />
+        </mesh>
+      )}
     </group>
   );
 };
